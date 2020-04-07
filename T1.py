ec2ab01525bd6d8afa529c51163c1f3654b420ac
The Supermanyam <60550869+Shivratna@users.noreply.github.com>
2020-04-08 01:12:06 +0530

    Update T1.py

--- T1.py
+++ T1.py
@@ -1,25 +1,25 @@
 #TRuck1
 import socket                
-import time 
+import time
+
 s = socket.socket()          
 port = 1241          
 
 s.connect(('127.0.0.1', port))
 s.setblocking(1)
+
 while True:
     dat = s.recv(1024).decode('utf-8')    
     t = dat.split(' ')
     loc = t[0]
     ti =int(t[1])
+    
     con = s.recv(1024).decode('utf-8')
     if con=='false':
         s.send(t[1].encode('utf-8'))
+        
     b=loc+' Truck1\n'
     print("Writing into file")
     s.send(b.encode('utf-8'))
     
-    
-    #msg=input()
-    #s.send(msg.encode('ascii'))
-    # close the connection 
 
